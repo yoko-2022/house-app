55c18fa2c5a8e1126163670de6d996e4f4c5138a
yoko-2022 <rilapy_029@yahoo.co.jp>
2023-04-25 13:15:14 +0900

    Chart.jsx等の追加

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\FormController; 
-use App\Http\Controllers\ReportController; 
+use App\Http\Controllers\FormController;
+use App\Http\Controllers\ReportController;
 use App\Http\Controllers\DeleteController;
+use App\Http\Controllers\GetTableDataController;
 
-Route::post('/form', [FormController::class, 'store']);
+Route::post("/form", [FormController::class, "store"]);
 
-Route::get('/report/{selectMonth}', [ReportController::class, 'input']);
+Route::get("/report/{selectMonth}", [ReportController::class, "input"]);
 
-Route::delete('/data/{id}', [DeleteController::class, 'destroy']);
+Route::delete("/data/{id}", [DeleteController::class, "destroy"]);
+
+Route::get("/gettabledata", [GetTableDataController::class, "index"]);
